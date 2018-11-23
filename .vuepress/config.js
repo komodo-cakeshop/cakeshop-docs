e98e6fe349fae68726fb369ff2e404713cff1486
imylomylo <imylomylo@gmail.com>
2018-11-23 19:29:30 +0100

    added existing komodo documenttion pages that were relevant for editing/review

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  host: '0.0.0.0',
   title: 'Cakeshop Docs',
   description: 'Just playing around',
   themeConfig: {
@@ -35,6 +36,8 @@ module.exports = {
 	collapsable: true,
 	children: [
 	  [ '/blockchain-starter-kit/introduction.html', 'Blockchain Starter Kit Introduction' ],
+	  [ '/blockchain-starter-kit/checklist-new-coins.rst.txt.html', 'Existing New Coibs Checklist' ],
+	  [ '/blockchain-starter-kit/setup-electrumX-server.rst.txt.html', 'Existing ElectrumX Setup' ],
 	  [ '/blockchain-starter-kit/ac-parameters.html', 'Blockchain Parameters' ],
 	  [ '/blockchain-starter-kit/configuration.html', 'Blockchain Configuration' ],
 	  [ '/blockchain-starter-kit/deployment.html', 'Blockchain Deployment' ],
@@ -53,7 +56,95 @@ module.exports = {
 	  [ '/custom-consensus/hello-world.html', 'Crypto Condition Hello World'],
 	  [ '/custom-consensus/cloud9.html', 'Crypto Condition Cloud 9 IDE']
 	]
-      }
+      },
+      {
+        title: 'Existing BarterDEX',
+        collapsable: true,
+        children: [
+         ['/existing/barterdex/home-barterDEX.rst.txt.html', 'BarterDEX Introduction'],
+         ['/existing/barterdex/get-listed-barterDEX.rst.txt.html', 'Get Listed on BarterDEX'],
+         ['/existing/barterdex/setup-electrumX-server.rst.txt.html', 'Setup ElectrumX Server']
+        ]
+      },{
+        title: 'Existing Building Software',
+        collapsable: true,
+        children: [
+         ['/existing/building-software/compile-marketmaker-binary-with-static-nanomsg-in-Linux.rst.txt.html', 'Compile Marketmaker Linux'],
+         ['/existing/building-software/compile-marketmaker-binary-with-static-nanomsg-in-MacOS.rst.txt.html', 'Compile Marketmaker OSX'],
+         ['/existing/building-software/debug-Komodo.rst.txt.html', 'Debug Komodo'],
+         ['/existing/building-software/install-barterDEX-CLI.rst.txt.html', 'Install BarterDEX CLI'],
+         ['/existing/building-software/install-Komodo-manually.rst.txt.html', 'Compile Komodo']
+        ]
+      },{
+        title: 'Existing Best Practices',
+        collapsable: true,
+        children: [
+         ['/existing/best-practices/note-exchanges.rst.txt.html', 'Crypto Exchange Note'],
+         ['/existing/best-practices/security-setup-Komodo-Node.rst.txt.html', 'Securely Setup Komodo Node'],
+         ['/existing/best-practices/setup-FR-full-relay-node-barterDEX.rst.txt.html', 'Setup Full Relay (FR) Node']
+        ]
+      },{
+        title: 'Existing CLI Operations',
+        collapsable: true,
+        children: [
+         ['/existing/cli-operations/activate-cc-independent-chain.rst.txt.html', 'Activatation Height Crypto Conditions'],
+         ['/existing/cli-operations/assetchains-guide-Komodo-Notary-Node.rst.txt.html', 'Assetchains For Notary Nodes'],
+         ['/existing/cli-operations/test-komodo-source-jl777-branch.rst.txt.html', 'Test Forking'],
+         ['/existing/cli-operations/test-use-write-integrate-cc.rst.txt.html', 'Create Crypto Conditions'],
+         ['/existing/cli-operations/useful-KMD-BTC-commands.rst.txt.html', 'KMD/BTC Commands'],
+         ['/existing/cli-operations/using-Key-Value.rst.txt.html', 'Using KV Store']
+        ]
+      },{
+        title: 'Existing Crypto Conditions',
+        collapsable: true,
+        children: [
+         ['/existing/cryptoconditions/introduction.rst.txt.html', 'CC Intro'],
+         ['/existing/cryptoconditions/test-use-write-integrate-cc.rst.txt.html', 'Overview'],
+         ['/existing/cryptoconditions/tutorial.rst.txt.html', 'Tutorial']
+        ]
+      },{
+        title: 'Existing Dev-HOWTO',
+        collapsable: true,
+        children: [
+         ['/existing/dev-howtos/activate-cc-independent-chain.rst.txt.html', 'Update name1'],
+         ['/existing/dev-howtos/add-Komodo-Assetchains-Agama-Desktop.rst.txt.html', 'Update name2'],
+         ['/existing/dev-howtos/assetchain-params.rst.txt.html', 'Update name3'],
+         ['/existing/dev-howtos/create-Komodo-Assetchain.rst.txt.html', 'Update name4'],
+         ['/existing/dev-howtos/debug-Komodo.rst.txt.html', 'Update name5'],
+         ['/existing/dev-howtos/example-asset-chains.rst.txt.html', 'Update name6'],
+         ['/existing/dev-howtos/generate-privkeys-third-party-coins-from-passphrase.rst.txt.html', 'Update name7'],
+         ['/existing/dev-howtos/test-use-write-integrate-cc.rst.txt.html', 'Update name8'],
+         ['/existing/dev-howtos/using-Key-Value.rst.txt.html', 'Update name9']
+        ]
+      },{
+        title: 'Existing Glossary',
+        collapsable: true,
+        children: [
+         ['/existing/glossary/assetchain-params.rst.txt.html', 'Assetchain Params'],
+         ['/existing/glossary/activate-cc-independent-chain.rst.txt.html', 'Activation Height CC'],
+         ['/existing/glossary/Smartaddress-FAQ.rst.txt.html', 'Smart Address FAQ'],
+         ['/existing/glossary/faq.rst.txt.html', 'FAQ']
+        ]
+      },{
+        title: 'Existing Guides',
+        collapsable: true,
+        children: [
+         ['/existing/guides/beginner-vps-linux-assetchain.rst.txt.html', 'VPS Getting Started'],
+         ['/existing/guides/create-Komodo-Assetchain.rst.txt.html', 'Create Blockchain'],
+         ['/existing/guides/dPOW-conf.rst.txt.html', 'dPoW Confirmations'],
+         ['/existing/guides/setup-Komodo-Notary-Node.rst.txt.html', 'Setup Notary Node'],
+         ['/existing/guides/Smartaddress-FAQ.rst.txt.html', 'Smartaddress FAQ'],
+         ['/existing/guides/using-JUMBLR.rst.txt.html', 'Using JUMBLR'],
+        ]
+      },{
+        title: 'Existing Index',
+        collapsable: true,
+        children: [
+         ['/existing/index/list-all-KomodoPlatform-Project-links.rst.txt.html', 'Project Resources & Repositories'],
+         ['/existing/index/info.rst.txt.html', 'Coin Index']
+        ]
+      },
     ]
   }
 }
+
---
+++ existing/index/list-all-KomodoPlatform-Project-links.rst.txt.md
@@ -0,0 +1,152 @@
+**********************************************************
+Collection of all KomodoPlatform Repositories and Releases
+**********************************************************
+
+**This page will link all related Komodo and SuperNET project source repositories and builds. Please let us know in** `Discord <https://komodoplatform.com/discord>`_ **if any information needs to be updated.**
+
+Agama (Desktop)
+===============
+
+Source code
+-----------
+
+* Backend: https://github.com/KomodoPlatform/Agama/
+* UI: https://github.com/KomodoPlatform/EasyDEX-GUI/
+
+Latest installer
+----------------
+
+* Linux: https://artifacts.supernet.org/latest/installer_linux.html
+* macOS: https://artifacts.supernet.org/latest/installer_osx.html
+* Windows: https://artifacts.supernet.org/latest/installer_windows.html
+
+Agama Mobile
+============
+
+* Source code: https://github.com/pbca26/agama-mobile
+* Release: https://github.com/pbca26/agama-mobile/releases
+
+Agama Web Wallet
+================
+
+* Source: https://github.com/pbca26/agama-web
+* Dependencies 1: https://github.com/pbca26/agama-wallet-lib
+* Dependencies 2:https://github.com/pbca26/electrum-proxy
+* Hosted: https://www.atomicexplorer.com/wallet/
+
+barterDEX CLI (normal)
+======================
+
+* Source code: https://github.com/jl777/SuperNET
+* Setup guide: :doc:`/barterDEX/install-barterDEX-CLI`
+* Knowledge base : :doc:`/home-barterDEX`
+
+barterDEX CLI (etomic)
+======================
+
+* Source code: https://github.com/artemii235/SuperNET/tree/etomic
+* Setup guide: :doc:`/barterDEX/install-ETOMIC-barterDEX`
+
+BarterDEX GUI
+=============
+
+* Source code: https://github.com/KomodoPlatform/BarterDEX
+* Release package: https://github.com/KomodoPlatform/BarterDEX/releases
+
+
+HyperDEX
+========
+
+* Source code: https://github.com/atomiclabs/hyperdex
+* Release package: https://github.com/atomiclabs/hyperdex/releases
+* Nightly bulds: https://github.com/atomiclabs/hyperdex-nightlies/releases/
+* Download Instructions: https://github.com/atomiclabs/hyperdex/blob/master/README.md
+
+CHIPS
+=====
+
+* Source code: https://github.com/jl777/chips3
+
+CHIPSln
+=======
+
+Source code: https://github.com/jl777/chipsln
+
+Coins
+=====
+
+* Source code: https://github.com/jl777/coins
+
+CryptoConditions
+================
+
+* Source code: https://github.com/jl777/komodo/tree/dev/src/cc
+* Mastering CryptoConditions: https://github.com/KomodoPlatform/Mastering_CryptoConditions
+
+dICO App
+========
+
+Needs an update after the final version is publicly available
+
+* Source code: https://github.com/ca333/atomicDEX
+
+Insight Explorer 
+================
+
+* All-in-one Insight explorer setup for Komodo and all assetchains: https://github.com/DeckerSU/komodo-explorers-install
+
+Komodo
+======
+
+Source code
+-----------
+
+* https://github.com/jl777/komodo
+
+Latest Binary
+-------------
+
+* https://github.com/KomodoPlatform/komodo/releases
+
+Komodo-Qt (KomodoOcean)
+=======================
+
+Source code
+-----------
+
+* https://github.com/ip-gpu/KomodoOcean
+* https://github.com/DeckerSU/KomodoOcean
+
+Release package
+---------------
+
+* https://github.com/DeckerSU/KomodoOcean/releases
+
+Marketmaker-cli
+===============
+
+* Source code: https://github.com/KomodoPlatform/marketmaker-cli
+* Release binary: https://github.com/eliezio/marketmaker-cli/releases
+
+Marketmaker 2.0
+===============
+
+* Source code: https://github.com/artemii235/SuperNET
+* MM2 Doc: https://github.com/artemii235/SuperNET/blob/master/iguana/exchanges/mm2.md
+
+Scaling Test
+============
+
+* Source code: https://github.com/blackjok3rtt/ScaleTestV2
+* Scaletest Containers: https://github.com/patchkez/scaletest_containers
+* Transaction Visualization: https://github.com/smk762/txscl_vis
+* Tx Blaster: https://github.com/Meshbits/TxBlaster
+* Speedtest: https://github.com/dwygit/komodotools/tree/master/dwy/speedtest
+* Signup Pad: http://pad.supernet.org/Stress_Test_Signups
+
+SuperNET, Iguana ``source code``
+================================
+
+Please refer to individual installation process
+
+* https://github.com/jl777/SuperNET
